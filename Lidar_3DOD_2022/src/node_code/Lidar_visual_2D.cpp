3e188ed5da7a481bfffbe15f9c1b237b795d1693
Dong-Young-Kim <bigdykim@gmail.com>
2022-05-27 12:37:06 +0900

    Update Lidar_visual_2D.cpp

--- Lidar_3DOD_2022/src/node_code/Lidar_visual_2D.cpp
+++ Lidar_3DOD_2022/src/node_code/Lidar_visual_2D.cpp
@@ -10,7 +10,7 @@ void show_process(const Lidar_3DOD_2022::obj_msg objs){
     cout << objs.objc << endl;
 
     cv::Mat src = cv::imread(img, cv::IMREAD_COLOR);
-    //cv::rectangle(src, cv::Rect(20,20,40,20), cv::Scalar(0,0,255), cv::FILLED, -1,0);
+    cv::rectangle(src, cv::Rect(20,20,40,20), cv::Scalar(0,0,255), 20, 8, 0);
 
     cv::imshow("Lidar Detected objs", src);
     cv::waitKey(1);
